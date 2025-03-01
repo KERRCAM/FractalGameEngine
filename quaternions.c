ea81c1f36756c8b8897a23ae8c52173800201594
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2025-03-01 18:33:16 +0000

    files renamed

--- quaternions.c
+++
@@ -1,2 +0,0 @@
-// quaternions describe roation, roation itself seperate thing
-// liekly turns into rotation.c and roation.h files that just also contain quaternions for operations
---
+++ rotations.c
@@ -0,0 +1,2 @@
+// also contain quaternions as part of operations for rotations
+// quaternions describe a roation but are seperate to how to show a roation of a certain object
