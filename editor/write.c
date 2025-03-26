6e75c93a28b9e3f97ed9987dca0c946cff9e58c6
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2025-03-26 00:39:02 +0000

    next steps todo

---
+++ editor/write.c
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+/* TODO:
+- Write saved levels into seperated .h each
+- Also on editor start up we need to initialise our structs with the information in existing .h level files
+- Move render code to seperate files maybe
+- also maybe try reduce duplicate code in input processor
+*/
